aed6b99fbf5c14062661c0fcf02a72bcb39fe7a5
samuel sonawane <sosam29@gmail.com>
2019-01-22 19:47:10 -0500

    nn code
     Initial commit
    	new file:   preceptron.py
    	new file:   preceptron_or.py

---
+++ preceptron_or.py
@@ -0,0 +1,12 @@
+from preceptron import Perceptron
+import numpy as np 
+
+X=np.array([[0,0],[0,1],[1,0],[1,1]])
+y = np.array([[0],[1],[1],[1]])
+
+p= Perceptron(X.shape[1], alpha=0.1)
+p.fit(X, y, epochs=10)
+
+for (x, target) in zip(X, y):
+    pred = p.predict(x)
+    print("[info] input{}, expected: {}, predicted: {}".format(x, target[0], pred))
