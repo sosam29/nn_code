44a58b1f957dfea35517421513d3403fa80d2e0f
samuel sonawane <sosam29@gmail.com>
2019-02-18 15:49:22 -0500

    using old model

--- cnn/usingOldModel.py
+++ cnn/usingOldModel.py
@@ -5,7 +5,9 @@ from keras.optimizers import SGD
 from keras.datasets import cifar10
 import matplotlib.pyplot as plt 
 import numpy as np 
+from keras.models import load_model
 
+# print()
 ((trainX, trainY),(testX, testY)) = cifar10.load_data()
 trainX = trainX.astype('float')/255.0
 testX = testX.astype('float')/255.0
@@ -15,11 +17,8 @@ trainY = lb.fit_transform(trainY)
 testY= lb.transform(testY)
 
 labelNames = ["Airplane","Automobile","bird","cat","deer","dog", "frog","horse","ship","duck"]
-opt =SGD(lr=0.005)
-model = ShallowNN.build(width=32, height=32, depth=3, classes=10)
-model.compile(opt, loss="categorical_crossentropy", metrics=["accuracy"])
 
-H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=50, epochs=40, verbose=-1)
+model =load_model('../cifar10.model.hdf5')
 
 print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=32)
@@ -39,6 +38,3 @@ plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
 plt.legend()
 plt.show()
-
-
-
